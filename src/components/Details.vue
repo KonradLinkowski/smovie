7e4a60700a82a6774088145405758e8885ea36b4
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-01-19 20:41:27 +0100

    Improved details view

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="lightbox" v-show="isOpen">
     <div style="width: 100%; height: 100%; position: fixed"  @click="close"></div>
-    <div class="details" style="z-index: 100">
-      <div class="poster">
-        <img v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie">
-        <img v-else src="https://placehold.it/500x750" alt="Movie">
+    <div class="details container" style="z-index: 100; flex-wrap: nowrap">
+      <div class="poster" style="margin: 10px; flex-shrink: 0">
+        <img class="border" v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie">
+        <img class="border" v-else src="https://placehold.it/500x750" alt="Movie">
       </div>
-      <p>{{ movie.title }}</p>
-      <p v-for="item in movie.genre_ids" :key="item.id">{{ getGenreName(item) }}</p>
+      <div style="padding:0">
+        <h2>{{ movie.title }}</h2>
+        <a :href="'https://www.themoviedb.org/movie/' + movie.id">https://www.themoviedb.org/movie/{{ movie.id }}</a>
+        <p>
+          {{ movie.overview }}
+        </p>
+        <p class="no-margin">
+          Gatunki
+          <ul>
+            <li v-for="(item, index) in movie.genres" :key="item.id">{{ movie.genres[index].name }}</li>
+          </ul>
+        </p>
+        <p class="no-margin">
+          Kraje produkcji
+          <ul>
+            <li v-for="(item, index) in movie.production_countries" :key="item.id">{{ movie.production_countries[index].name }}</li>
+          </ul>
+        </p>
+        <p class="no-margin">
+          Firmy produkcyjne
+          <ul>
+            <li v-for="(item, index) in movie.production_companies" :key="item.id">{{ movie.production_companies[index].name }}</li>
+          </ul>
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import eventHub from '../utils/eventhub'
-import { getGenres } from '../utils/api'
+import { getMovie } from '../utils/api'
 export default {
   data() {
     return {
@@ -25,28 +48,21 @@ export default {
   },
   created: function() {
     eventHub.$on('show-details', this.open)
-    getGenres()
-    .then(genres => {
-      this.genres = genres
-    })
-    .catch(err => {
-      console.error(err)
-    })
   },
   methods: {
     open(movie) {
-      console.log(movie.genre_ids)
-      this.movie = movie
+      getMovie(movie.id)
+      .then(details => {
+        this.movie = Object.assign(movie, details)
+      })
+      .catch(err => {
+        console.error(err)
+      })
       this.isOpen = true;
     },
     close() {
       this.isOpen = false;
     },
-    getGenreName(id) {
-      return this.genres.filter(genre => {
-        return genre.id == id
-      })[0].name
-    }
   }
 }
 </script>
--- src/utils/api.js
+++ src/utils/api.js
@@ -10,6 +10,7 @@ export function getPopularMovies(params) {
       url: serverURL + 'movie/popular',
       params: {
         api_key: key,
+        language: 'pl',
         page: params.page
       }
     })
@@ -22,13 +23,33 @@ export function getPopularMovies(params) {
   })
 }
 
+export function getMovie(id) {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'GET',
+      url: serverURL + 'movie/' + id,
+      params: {
+        api_key: key,
+        language: 'pl',
+      }
+    })
+    .then(({data}) => {
+      resolve(data)
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
+
 export function getGenres() {
   return new Promise((resolve, reject) => {
     axios({
       method: 'GET',
       url: serverURL + 'genre/movie/list',
       params: {
-        api_key: key
+        api_key: key,
+        language: 'pl',
       }
     })
     .then(({data}) => {
