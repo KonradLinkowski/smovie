8cbb38e52f0e1fcc33aac1754db419719405b580
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-01-23 22:29:23 +0100

    CSS changes

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,18 +1,18 @@
 <template>
-  <article class="movie border poster shadow-bot hvr-float clickable" tabindex="0" @click="openDetails">
-    <img v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie">
-    <img v-else src="https://placehold.it/500x750" alt="Movie">
-    <aside>
-      <h1 class="fnt-md" style="height:2.5rem; padding: 10px">{{ movie.title }}</h1>
-      <div style="width:100%; height: 3rem; margin-top: 10px; padding: 10px;">
-        <p class="fnt-sm no-margin">Popularność: {{ movie.popularity }}</p>
-        <p class="fnt-sm no-margin">Liczba głosów: {{ movie.vote_count }}</p>
-      </div>
-      <div class="footer" style="padding: 10px;">
-        <span>{{ movie.release_date }}</span>
-        <span style="float: right">{{ movie.vote_average }}</span>
-      </div>
-    </aside>
+  <article class="movie border poster shadow-bot hvr-float clickable container-vert" tabindex="0" @click="openDetails">
+    <div>
+      <img v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie">
+      <img v-else src="https://placehold.it/500x750" alt="Movie">
+    </div>
+    <div class="flex-grow movie-details">
+      <h1 class="">{{ movie.title }}</h1>
+      <p class="">Popularność: {{ movie.popularity }}</p>
+      <p class="">Liczba głosów: {{ movie.vote_count }}</p>
+    </div>
+    <div class="container flex-space-between movie-footer">
+      <span>{{ movie.release_date }}</span>
+      <span>{{ movie.vote_average }}</span>
+    </div>
   </article>
 </template>
 
