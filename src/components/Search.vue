a05d7cc688574f9cceab71a1c7748941d2385088
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-01-24 19:02:50 +0100

    Home button

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-bar container flex-center-vert max-width rounded shadow-bot">
     <div class="container site-width center-hor">
-      <!--<button @click="goHome">Home</button>-->
+      <button @click="goHome"><i class="material-icons">home</i></button>
       <input class="search flex-grow" @keyup.enter="search" v-model="message" placeholder="Szukaj">
       <button :disabled="message < 1" @click="search"><i class="medium material-icons">search</i></button>
     </div>
