add710727c9571d2a7b1a41953f9f4755d668b8b
Maira Garcia <garciamaira524@yahoo.com>
2021-10-08 01:02:46 -0700

    fix styling on movie card

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -3,39 +3,41 @@
     <div class="position-fixed max-width max-height top-0 clickable" @click="close"></div>
     <div class="details container flex-start">
       <div class="close-btn" @click="close"><i class="material-icons">close</i></div>
-      <div class="poster flex-no-shrink margin-right">
-        <img class="border" v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie">
-        <img class="border" v-else src="https://placehold.it/500x750" alt="Movie">
-      </div>
       <div class="container-vert flex-wrap flex-start">
         <div class="flex-strech-self">
           <h2>{{ movie.title }}</h2>
           <h3 class="italic">{{ movie.original_title }}</h3>
+          <br>
           <a :href="'https://www.themoviedb.org/movie/' + movie.id">https://www.themoviedb.org/movie/{{ movie.id }}</a>
-          <p class="flex-strech-self margin-5">
+          <br>
+          <p class="flex-strech-self">
             {{ movie.overview }}
           </p>
-          <div class="container flex-wrap flex-start">
+          <div class="container flex-wrap bullet-pts">
             <p class="margin-5" v-if="movie.genres && movie.genres.length">
-              Gatunki
+              <span class="small-bold">Gatunki</span>
               <ul>
                 <li v-for="(item, index) in movie.genres" :key="item.id">{{ movie.genres[index].name }}</li>
               </ul>
             </p>
             <p class="margin-5" v-if="movie.production_countries && movie.production_countries.length">
-              Kraje produkcji
+              <span class="small-bold">Kraje produkcji</span>
               <ul>
                 <li v-for="(item, index) in movie.production_countries" :key="item.id">{{ movie.production_countries[index].name }}</li>
               </ul>
             </p>
             <p class="margin-5" v-if="movie.production_companies && movie.production_companies.length">
-              Firmy produkcyjne
+              <span class="small-bold">Firmy produkcyjne</span>
               <ul>
                 <li v-for="(item, index) in movie.production_companies" :key="item.id">{{ movie.production_companies[index].name }}</li>
               </ul>
             </p>
           </div>
         </div>
+      </div>
+      <div class="poster poster-info flex-no-shrink margin-right">
+        <img class="border" v-if="movie.poster_path" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie">
+        <img class="border" v-else src="https://placehold.it/500x750" alt="Movie">
       </div>
     </div>
   </div>
